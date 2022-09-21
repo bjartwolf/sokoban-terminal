e9c25cb56cf4b4923f26c34ead93269fc007ebf9
bjorn-einar-bjartnes-4ss <bjartnes@gmail.com>
2022-09-21 23:13:35 +0200

    Indentation of strings

--- test/game.tests/Tests.fs
+++ test/game.tests/Tests.fs
@@ -6,3 +6,9 @@ open Xunit
 [<Fact>]
 let ``Game is not blank`` () =
     Assert.True(game.gameControl.getGame().Length > 4)
+
+[<Fact>]
+let ``Indented strings are not long as long as one moves them in`` () =
+    let smallBoard = game.gameControl.getGameSmall()
+    let length = smallBoard.Length
+    Assert.True(smallBoard.Length <= 4 + 2*2)
