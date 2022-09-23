c7c51d26ce2f285bb7e26ed6e29f9fd2280dd5ce
bjorn-einar-bjartnes-4ss <bjartnes@gmail.com>
2022-09-23 17:41:36 +0200

    Add constants

--- src/game/Library.fs
+++ src/game/Library.fs
@@ -15,3 +15,14 @@ module gameControl =
     let getGameSmall () = @"
 ##
 ##"
+    // http://www.sokobano.de/wiki/index.php?title=Level_format
+    let wall = '#'
+    let player = '@'
+    let player_on_goal_square = '@'
+    let box = '$'
+    let box_on_goal_square = '*'
+    let goal_square = '.'
+    let floor = ' '
+
+
+
