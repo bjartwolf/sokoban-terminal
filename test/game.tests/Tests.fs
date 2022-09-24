323f5017cdf8a3a9b561d2939359402e70fb2b75
bjorn-einar-bjartnes-4ss <bjartnes@gmail.com>
2022-09-24 10:03:08 +0200

    Add failing test

--- test/game.tests/Tests.fs
+++ test/game.tests/Tests.fs
@@ -61,6 +61,17 @@ let moveLeft_PlayerCanMove_PlayerMovesLeft () =
     Assert.True(positionAreEqual) 
 
 [<Fact>]
+let moveRight_PlayerCantMove_NothingChanges() =
+    let boardBefore = @"
+   
+ @"
+    let board = game.parseBoard(boardBefore)
+
+    let newBoard = game.movePlayer board 'l'
+    let positionAreEqual =  (board = newBoard)
+    Assert.True(positionAreEqual) 
+
+[<Fact>]
 let moveUp_PlayerCanMove_PlayerMovesUp () =
     let boardBefore = @"
    
