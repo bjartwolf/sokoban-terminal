d00fb2467dfc9556710a9ebcdf6718f289554e12
bjorn-einar-bjartnes-4ss <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2022-10-01 20:07:19 +0200

    Add buttons

--- sokoban-js/src/App.fs
+++ sokoban-js/src/App.fs
@@ -8,10 +8,31 @@ let mutable count = 0
 // Get a reference to our button and cast the Element to an HTMLButtonElement
 let downButton = document.querySelector(".button-down") :?> Browser.Types.HTMLButtonElement
 let upButton = document.querySelector(".button-up") :?> Browser.Types.HTMLButtonElement
-let gameScreen = document.querySelector(".game-screen") :?> Browser.Types.HTMLParagraphElement
+let leftButton = document.querySelector(".button-left") :?> Browser.Types.HTMLButtonElement
+let rightButton = document.querySelector(".button-right") :?> Browser.Types.HTMLButtonElement
+let gameScreen = document.querySelector(".game-screen") :?> Browser.Types.HTMLDivElement
 
-gameScreen.textContent <- game.init(3) 
+let boardNr = 3 
+let mutable moves: string = ""
+gameScreen.textContent <- game.init(boardNr) 
 
 // Register our listener
 downButton.onclick <- fun _ ->
-    downButton.innerText <- sprintf "You cxlicked: %i time(s)" count
+    let (board, history)= game.attemptMove(boardNr, moves, 'd')
+    gameScreen.textContent <- board
+    moves <- history
+
+upButton.onclick <- fun _ ->
+    let (board, history)= game.attemptMove(boardNr, moves, 'u')
+    gameScreen.textContent <- board
+    moves <- history
+
+rightButton.onclick <- fun _ ->
+    let (board, history)= game.attemptMove(boardNr, moves, 'r')
+    gameScreen.textContent <- board
+    moves <- history
+
+leftButton.onclick <- fun _ ->
+    let (board, history)= game.attemptMove(boardNr, moves, 'l')
+    gameScreen.textContent <- board
+    moves <- history
