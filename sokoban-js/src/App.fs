935bd2b88bbd1f4b922a4437aa943bdc1d8c9aa2
bjorn-einar-bjartnes-4ss <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2022-10-01 21:23:59 +0200

    Update App.fs

--- sokoban-js/src/App.fs
+++ sokoban-js/src/App.fs
@@ -5,14 +5,6 @@ open sokoban
 open Browser.Types
 open System
 
-// Mutable variable to count the number of times we clicked the button
-let mutable count = 0
-
-// Get a reference to our button and cast the Element to an HTMLButtonElement
-let downButton = document.querySelector(".button-down") :?> Browser.Types.HTMLButtonElement
-let upButton = document.querySelector(".button-up") :?> Browser.Types.HTMLButtonElement
-let leftButton = document.querySelector(".button-left") :?> Browser.Types.HTMLButtonElement
-let rightButton = document.querySelector(".button-right") :?> Browser.Types.HTMLButtonElement
 let gameScreen = document.querySelector(".game-screen") :?> Browser.Types.HTMLDivElement
 
 let boardNr = 3 
@@ -24,12 +16,5 @@ let go (dir: Char) =
     moves <- history
 
 gameScreen.textContent <- game.init(boardNr) 
-
-
-downButton.onclick <- fun _ -> go ('d')
-upButton.onclick <- fun _ -> go 'u'
-rightButton.onclick <- fun _ -> go 'r' 
-leftButton.onclick <- fun _ -> go 'l'
-
 document.onkeydown <- fun (keyEvent: KeyboardEvent) -> go (keyEvent.key |> Seq.head)
                                  
