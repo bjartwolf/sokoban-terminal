2d1c99a63c2bbbdaf25ecf0b7a41cd95de276cf9
bjorn-einar-bjartnes-4ss <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2022-10-01 13:47:28 +0200

    Sort of plays a bit

--- sokoban-js/src/App.fs
+++ sokoban-js/src/App.fs
@@ -1,14 +1,17 @@
 module App
 
 open Browser.Dom
-
+open sokoban
 // Mutable variable to count the number of times we clicked the button
 let mutable count = 0
 
 // Get a reference to our button and cast the Element to an HTMLButtonElement
-let myButton = document.querySelector(".my-button") :?> Browser.Types.HTMLButtonElement
+let downButton = document.querySelector(".button-down") :?> Browser.Types.HTMLButtonElement
+let upButton = document.querySelector(".button-up") :?> Browser.Types.HTMLButtonElement
+let gameScreen = document.querySelector(".game-screen") :?> Browser.Types.HTMLParagraphElement
+
+gameScreen.textContent <- game.init(3) 
 
 // Register our listener
-myButton.onclick <- fun _ ->
-    count <- count + 1
-    myButton.innerText <- sprintf "You clicked: %i time(s)" count
+downButton.onclick <- fun _ ->
+    downButton.innerText <- sprintf "You cxlicked: %i time(s)" count
