0827f34936a2505ad8c0d22d56591c790a078195
bjorn-einar-bjartnes-4ss <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2022-10-02 19:46:41 +0200

    Trying to run tk inter and it does at least start

--- src/sokopy/Program.fs
+++ src/sokopy/Program.fs
@@ -1,5 +1,7 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 open sokoban
+open Fable.Python.TkInter
+
 printfn "Game 1"
 let finalState,_= game.attemptMove(1,"urduullddrd",'l') 
 printf "%A" finalState 
@@ -9,3 +11,7 @@ printfn ""
 printfn "Game 3"
 let finalState3,_= game.attemptMove(3,"urduullddrd",'l') 
 printf "%A" finalState3 
+
+let root = Tk()
+root.title ("Fable Python Rocks on Tkinter!")
+root.mainloop ()
--- src/sokopy/program.py
+++ src/sokopy/program.py
@@ -1,12 +1,14 @@
+from __future__ import annotations
+from tkinter import Tk
 from typing import Tuple
 from game.library import attempt_move
 from fable_modules.fable_library.string import (to_console, printf)
 
 to_console(printf("Game 1"))
 
-pattern_input_00404: Tuple[str, str] = attempt_move(1, "urduullddrd", "l")
+pattern_input_00406: Tuple[str, str] = attempt_move(1, "urduullddrd", "l")
 
-final_state: str = pattern_input_00404[0]
+final_state: str = pattern_input_00406[0]
 
 to_console(printf("%A"))(final_state)
 
@@ -14,9 +16,15 @@ to_console(printf(""))
 
 to_console(printf("Game 3"))
 
-pattern_input_004010_002D1: Tuple[str, str] = attempt_move(3, "urduullddrd", "l")
+pattern_input_004012_002D1: Tuple[str, str] = attempt_move(3, "urduullddrd", "l")
 
-final_state3: str = pattern_input_004010_002D1[0]
+final_state3: str = pattern_input_004012_002D1[0]
 
 to_console(printf("%A"))(final_state3)
+
+root: Tk = Tk()
+
+root.title("Fable Python Rocks on Tkinter!")
+
+root.mainloop()
 
