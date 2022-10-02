ea6c80b442af722487f92fe27b0806d2c5755111
bjorn-einar-bjartnes-4ss <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2022-10-02 21:46:36 +0200

    Playableish

--- src/sokopy/pygui.py
+++ src/sokopy/pygui.py
@@ -1,32 +1,87 @@
 ﻿from tkinter import * 
 from game.library import *
 
-print("game 1 go")
 gameNr = 3
+global board
 board = init(gameNr)
-print(board)
 global history
 history = ""
 
 tk = Tk()
-tk.geometry("750x280")
+tk.geometry("500x500")
 
-canvas= Canvas(tk, width= 1000, height= 750, bg="blue")
+canvas= Canvas(tk, width= 500, height= 500, bg="blue")
 
-tekst = canvas.create_text(300, 100, text=board, fill="white", font=('TkFixedFont'))
+tekst = canvas.create_text(250, 250, text=board, fill="white", font=('TkFixedFont'))
 canvas.pack()
 
 def left(self):
-     newboard, newhistory = attempt_move(gameNr, history, "l")
-     board = newboard
+     global history, board, gameNr
+     board, history = attempt_move(gameNr, history, "l")
      canvas.itemconfig(tekst,text = board)
      tk.update()
+     return board, history
 
-tk.bind('l',left)
+def right(self):
+     global history, board, gameNr
+     board, history = attempt_move(gameNr, history, "r")
+     canvas.itemconfig(tekst,text = board)
+     tk.update()
+     return board, history
+
+def up(self):
+     global history, board, gameNr
+     board, history = attempt_move(gameNr, history, "u")
+     canvas.itemconfig(tekst,text = board)
+     tk.update()
+     return board, history
+
+def down(self):
+     global history, board, gameNr
+     board, history = attempt_move(gameNr, history, "d")
+     canvas.itemconfig(tekst,text = board)
+     tk.update()
+     return board, history
+
+def board0(self):
+    global history, board, gameNr
+    history = ""
+    gameNr = 0
+    board = init(gameNr) 
+    canvas.itemconfig(tekst,text = board)
+    tk.update()
+    return history, board, gameNr
+
+def board1(self):
+    global history, board, gameNr
+    history = ""
+    gameNr = 1
+    board = init(gameNr) 
+    canvas.itemconfig(tekst,text = board)
+    tk.update()
+    return history, board, gameNr
+
+def board3(self):
+    global history, board, gameNr
+    history = ""
+    gameNr = 3
+    board = init(gameNr) 
+    canvas.itemconfig(tekst,text = board)
+    tk.update()
+    return history, board, gameNr
+
+
+
+tk.bind('a',left)
+tk.bind('h',left)
+tk.bind('w',up)
+tk.bind('k',up)
+tk.bind('f',right)
+tk.bind('l',right)
+tk.bind('s',down)
+tk.bind('j',down)
+tk.bind('0',board0)
+tk.bind('1',board1)
+tk.bind('3',board3)
 
 tk.mainloop ()
-#while True:
-#    cmd = input("Skrive inn l,r,u,d,b")
-#    board, history = attempt_move(gameNr, history, cmd)
-#    print(board)
-#    print(history)
